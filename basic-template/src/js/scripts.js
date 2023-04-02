af8aac2458f259b1c50b283bf3a5f1a129a1864c
Nik_02 <ghostwarriorn@gmail.com>
2023-04-02 18:58:36 +0500

    update basic template

--- basic-template/src/js/scripts.js
+++ basic-template/src/js/scripts.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+// import * as dat from 'dat.gui'
 
 const renderer = new THREE.WebGLRenderer()
 renderer.shadowMap.enabled = true
@@ -21,47 +22,138 @@ const orbit = new OrbitControls(camera, renderer.domElement)
 camera.position.set(10, 10, 10)
 orbit.update()
 
-// grid
-// const grid = new THREE.GridHelper(30, 30)
-// scene.add(grid)
 
-// light
+///////////////// axs helper /////////////////
+// const axesHelper = new THREE.AxesHelper(3)
+// scene.add(axesHelper)
+
+
+///////////////// grid helper /////////////////
+// const gridHelper = new THREE.GridHelper(30)
+// scene.add(gridHelper)
+
+
+///////////////// ambient light /////////////////
 // const ambientLight = new THREE.AmbientLight('#ffffff', 0.8)
 // ambientLight.position.set(0, 20, 0)
 // scene.add(ambientLight)
 
 
-// create three js objects
-// const groundGeo = new THREE.PlaneGeometry(30, 30)
-// const groundMat = new THREE.MeshBasicMaterial({
-//     color: 0xffffff,
-//     side: THREE.DoubleSide,
-//     wireframe: true
-// })
-// const groundMesh = new THREE.Mesh(groundGeo, groundMat)
-// scene.add(groundMesh)
+///////////////// directional light /////////////////
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+// scene.add(directionalLight)
+// directionalLight.position.set(-30, 50, 0)
+// directionalLight.castShadow = true
+// directionalLight.shadow.camera.bottom = -12
+
+///////////////// dr-light helper /////////////////
+// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
+// scene.add(dLightHelper)
+
+///////////////// dr-light shadow helper /////////////////
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(dLightShadowHelper)
 
 
-// const boxGeo = new THREE.BoxGeometry(2,2,2)
-// const boxMat = new THREE.MeshBasicMaterial({
-//     color: 0x00ff00,
-//     side: THREE.DoubleSide,
-//     wireframe: true
-// })
-// const boxMesh = new THREE.Mesh(boxGeo, boxMat)
+///////////////// spotlight /////////////////
+// const spotLight = new THREE.SpotLight('#ffffff')
+// scene.add(spotLight)
+// spotLight.position.set(-100, 100, 0)
+// spotLight.castShadow = true
+// spotLight.angle = 0.2
+
+///////////////// sLight helper /////////////////
+// const sLightHelper = new THREE.SpotLightHelper(spotLight)
+// scene.add(sLightHelper)
+
+
+///////////////// fog /////////////////
+// scene.fog = new THREE.Fog('#ffffff', 0, 200)
+// scene.fog = new THREE.FogExp2('#ffffff', 0.01)
+
+
+///////////////// texture loaders /////////////////
+// const textureLoader = new THREE.TextureLoader()
+// scene.background = textureLoader.load(img03)
+
+// const cubeTextureLoader = new THREE.CubeTextureLoader()
+// scene.background = cubeTextureLoader.load([img, img, img, img, img, img])
+
+
+///////////////// plane /////////////////
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(30, 30),
+//     new THREE.MeshStandardMaterial({
+//         color: 0xFFFFFF,
+//         side: THREE.DoubleSide
+//     }))
+// scene.add(plane)
+// plane.rotation.x = -0.5 * Math.PI
+// plane.receiveShadow = true
+
+
+///////////////// box /////////////////
+// const boxMesh = new THREE.Mesh(
+//     new THREE.BoxGeometry(2,2,2),
+//     new THREE.MeshBasicMaterial({
+//         color: 0x00ff00,
+//         side: THREE.DoubleSide,
+//         wireframe: true
+//     })
+// )
 // scene.add(boxMesh)
 
-// const sphereGeo = new THREE.SphereGeometry(2)
-// const sphereMat = new THREE.MeshBasicMaterial({
-//     color: 0xff0000,
-//     side: THREE.DoubleSide,
-//     wireframe: true
-// })
-// const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat)
+
+///////////////// sphere /////////////////
+// const sphereMesh = new THREE.Mesh(
+//     new THREE.SphereGeometry(2),
+//     new THREE.MeshBasicMaterial({
+//         color: 0xff0000,
+//         side: THREE.DoubleSide,
+//         wireframe: true
+//     })
+// )
 // scene.add(sphereMesh)
 
 
-const animate = () => {
+///////////////// import 3d models /////////////////
+// const assetLoader = new GLTFLoader()
+// assetLoader.load(MODEL_NAME.href, (gltf) => {
+//     const model = gltf.scene
+//     model.scale.set(2,2,2)
+//     scene.add(model)
+//     model.position.set(-12, 0, 10)
+// }, undefined, (err) => {
+//     console.log(err)
+// })
+
+
+///////////////// gui /////////////////
+// const gui = new dat.GUI();
+// const options = {
+//
+// }
+// gui.addColor(options, '').onChange((e) => {
+//     // change color (...color.set(e))
+// })
+// gui.add(options, '').onChange((e) => {
+//     // change value (...= e)
+// })
+
+
+///////////////// get mouse pos /////////////////
+// const mousePosition = new THREE.Vector2()
+// window.addEventListener('mousemove', (e) => {
+//     mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1
+//     mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1
+// })
+
+
+
+// const clock = new THREE.Clock()
+
+const animate = (time) => {
+
 
     renderer.render(scene, camera)
 }
