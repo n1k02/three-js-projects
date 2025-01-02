8a660fbfc39771d7ffdc0a21c41e8ecfa0df7be0
_n1k_02_ <ghostwarriorn@gmail.com>
2025-01-02 18:47:24 +0500

    refreshh

--- car-physics/riged-vehicle-physics/src/js/scripts.js
+++ car-physics/riged-vehicle-physics/src/js/scripts.js
@@ -70,10 +70,10 @@ const wheelMaterial = new CANNON.Material('wheelMaterial');
 const down = new CANNON.Vec3(0, -1, 0)
 
 const wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, groundBody.material, {
-    friction: 0.3,
-    restitution: 1
+    friction: 0.4,
+    restitution: 0
 });
-// physicsWorld.addContactMaterial(wheelGroundContactMaterial)
+physicsWorld.addContactMaterial(wheelGroundContactMaterial)
 
 
 const wheelBody1 = new CANNON.Body({mass, material: wheelMaterial})
@@ -149,47 +149,45 @@ vehicle.addToWorld(physicsWorld)
 
 //controls
 document.addEventListener('keydown', (event) => {
-    const maxSteerVal = Math.PI / 5
-    const maxForce = 200
+    // const maxSteerVal = Math.PI / 5
+    const maxSteerVal = 0.4
+    const maxForce = 400
 
-    switch (event.key) {
-        case 'w':
+    switch (event.code) {
+        case 'KeyW':
             vehicle.setWheelForce(maxForce, 2)
             vehicle.setWheelForce(maxForce, 3)
             break
-        case 's':
+        case 'KeyS':
             vehicle.setWheelForce(-maxForce / 1.5, 2)
             vehicle.setWheelForce(-maxForce / 1.5, 3)
             break
-        case 'a':
+        case 'KeyA':
             vehicle.setSteeringValue(maxSteerVal, 0)
             vehicle.setSteeringValue(maxSteerVal, 1)
             break
-        case 'd':
+        case 'KeyD':
             vehicle.setSteeringValue(-maxSteerVal, 0)
             vehicle.setSteeringValue(-maxSteerVal, 1)
-            break
-        case 'space':
-
             break
     }
 })
 document.addEventListener('keyup', (event) => {
 
-    switch (event.key) {
-        case 'w':
+    switch (event.code) {
+        case 'KeyW':
             vehicle.setWheelForce(0, 2)
             vehicle.setWheelForce(0, 3)
             break
-        case 's':
+        case 'KeyS':
             vehicle.setWheelForce(0, 2)
             vehicle.setWheelForce(0, 3)
             break
-        case 'a':
+        case 'KeyA':
             vehicle.setSteeringValue(0, 0)
             vehicle.setSteeringValue(0, 1)
             break
-        case 'd':
+        case 'KeyD':
             vehicle.setSteeringValue(0, 0)
             vehicle.setSteeringValue(0, 1)
             break
@@ -199,7 +197,7 @@ document.addEventListener('keyup', (event) => {
 
 const cannonDebugger = new Debugger(scene, physicsWorld)
 
-const timeStep = 1 / 60
+const timeStep = 1 / 120
 
 const animate = () => {
     physicsWorld.step(timeStep)
